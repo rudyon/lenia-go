82131ba596693291d8e67c546213b3be0e96ad55
rudyon <keremberket@gmail.com>
2023-12-01 04:01:27 +0300

    ok it actually works now

--- main.go
+++ main.go
@@ -8,20 +8,37 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-func main() {
-	const width, height int32 = 800, 800
-	const world_width, world_height int32 = 100, 100
-	const scaling_value int32 = height / world_height
+const (
+	width         int32 = 800
+	height        int32 = 800
+	worldWidth    int32 = 100
+	worldHeight   int32 = 100
+	scalingValue  int32 = height / worldHeight
+	randomSquares int   = 1
+	squareSize    int   = 40
+)
 
-	world := initWorld(world_height, world_width)
+var (
+	ra     float64 = 11
+	alphaN float64 = 0.028
+	alphaM float64 = 0.147
+	b1     float64 = 0.278
+	b2     float64 = 0.365
+	d1     float64 = 0.267
+	d2     float64 = 0.445
+	dt     float64 = 0.05
+)
+
+func main() {
+	world := initWorld(worldHeight, worldWidth, randomSquares, squareSize)
 
 	rl.InitWindow(width, height, "lenia-go")
 	rl.SetConfigFlags(rl.FlagVsyncHint)
 
 	for !rl.WindowShouldClose() {
-		world = updateWorld(world, int(world_height), int(world_width))
+		world = updateWorld(world, int(worldHeight), int(worldWidth))
 
-		drawWorld(world, int(scaling_value))
+		drawWorld(world, int(scalingValue))
 	}
 }
 
@@ -38,7 +55,7 @@ func clamp(x, max, min float64) float64 {
 	return x
 }
 
-func drawWorld(world [][]float64, scaling_value int) {
+func drawWorld(world [][]float64, scalingValue int) {
 	rl.BeginDrawing()
 	rl.ClearBackground(rl.Black)
 
@@ -46,26 +63,36 @@ func drawWorld(world [][]float64, scaling_value int) {
 		for j := 0; j < len(world[0]); j++ {
 			alpha := world[i][j]
 			color := rl.ColorAlpha(rl.White, float32(alpha))
-			rl.DrawRectangleV(rl.NewVector2(float32(i*scaling_value), float32(j*scaling_value)), rl.NewVector2(float32(scaling_value), float32(scaling_value)), color)
+			rl.DrawRectangleV(rl.NewVector2(float32(i*scalingValue), float32(j*scalingValue)), rl.NewVector2(float32(scalingValue), float32(scalingValue)), color)
 		}
 	}
 
 	rl.EndDrawing()
 }
 
-func initWorld(world_height, world_width int32) [][]float64 {
-	var world = make([][]float64, world_height)
+func initWorld(worldHeight, worldWidth int32, numSquares, squareSize int) [][]float64 {
+	world := make([][]float64, worldHeight)
 	for i := range world {
-		world[i] = make([]float64, world_width)
-		for j := range world[i] {
-			world[i][j] = float64(rand.Float64())
+		world[i] = make([]float64, worldWidth)
+	}
+
+	// Initialize random squares
+	for s := 0; s < numSquares; s++ {
+		startX := rand.Intn(int(worldWidth) - squareSize + 1)
+		startY := rand.Intn(int(worldHeight) - squareSize + 1)
+
+		for i := 0; i < squareSize; i++ {
+			for j := 0; j < squareSize; j++ {
+				world[startY+j][startX+i] = rand.Float64()
+			}
 		}
 	}
+
 	return world
 }
 
 func sigma1(x, a float64) float64 {
-	return 1.0 / +math.Exp(-(x-a)*4/0.028)
+	return 1.0 / (1.0 + math.Exp(-(x-a)*4/alphaN))
 }
 
 func sigma2(x, a, b float64) float64 {
@@ -82,9 +109,9 @@ func s(n, m, b1, b2, d1, d2 float64) float64 {
 
 func updateWorld(world [][]float64, width, height int) [][]float64 {
 	const b1, b2, d1, d2 float64 = 0.278, 0.365, 0.267, 0.445
-	next_world := make([][]float64, len(world))
-	for i := range next_world {
-		next_world[i] = make([]float64, len(world[0]))
+	nextWorld := make([][]float64, len(world))
+	for i := range nextWorld {
+		nextWorld[i] = make([]float64, len(world[0]))
 	}
 
 	var wg sync.WaitGroup
@@ -93,10 +120,10 @@ func updateWorld(world [][]float64, width, height int) [][]float64 {
 		go func(i int) {
 			defer wg.Done()
 			for j := 0; j < len(world[0]); j++ {
-				outer_kernel := calculateOuterKernel(world, i, j, 11, width, height)
-				inner_kernel := calculateInnerKernel(world, i, j, 11/3, width, height)
+				outerKernel := calculateOuterKernel(world, i, j, int(ra-1), width, height)
+				innerKernel := calculateInnerKernel(world, i, j, int(ra-1)/3, width, height)
 
-				next_world[i][j] = s(outer_kernel, inner_kernel, b1, b2, d1, d2)
+				nextWorld[i][j] = 2*s(outerKernel, innerKernel, b1, b2, d1, d2) - 1
 			}
 		}(i)
 	}
@@ -104,7 +131,7 @@ func updateWorld(world [][]float64, width, height int) [][]float64 {
 
 	for i := range world {
 		for j := range world[i] {
-			world[i][j] += 1 * next_world[i][j]
+			world[i][j] += dt * nextWorld[i][j]
 			world[i][j] = clamp(world[i][j], 1, 0)
 		}
 	}
